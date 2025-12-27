846d8c30a870d2a13fe88bf2b9a2217373620e76
mamdouh <Mamdooh@users.noreply.github.com>
2025-12-27 20:13:55 +0300

    Fix deploy to sync files into container

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
+    <!-- Style s -->
     @if(env('APP_ENV') === 'local')
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @else
