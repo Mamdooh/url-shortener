b0eafe02a4f786274d9288cb099afbc52917cf60
mamdouh <Mamdooh@users.noreply.github.com>
2025-12-29 11:31:59 +0300

    Update homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Mr. Mazen - App test 29-12
+                    URL Shortener AWS and Azure Deploy Test
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
