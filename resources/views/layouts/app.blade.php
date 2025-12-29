34daf0f82edcc220fcf6d97d5f0fcfdb5bea272f
mamdouh <Mamdooh@users.noreply.github.com>
2025-12-29 11:33:29 +0300

    Added mr mazen name to homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    URL Shortener AWS and Azure Deploy Test
+                    Mr Mazen Online app for URL Short on azure and AWS
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
