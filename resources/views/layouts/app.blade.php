eb70e013e8e8b7c37ea2c8197492760ec7e50c91
mamdouh <Mamdooh@users.noreply.github.com>
2025-12-30 09:15:19 +0300

    rerun again pipeline

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Mmdouh URL Shortener v{{ config('version.version', 'local') }}
+                    Mmdouh URL Shortener v{{ config('version.version', 'local') }} 
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
